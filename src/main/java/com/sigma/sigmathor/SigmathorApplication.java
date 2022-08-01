32e91fe1c234b5dbbd8461433688cd41493d8912
sigma <sigma.dev.2022@gmail.com>
2022-08-01 20:01:14 -0300

    Primeiro commit

---
+++ src/main/java/com/sigma/sigmathor/SigmathorApplication.java
@@ -0,0 +1,13 @@
+package com.sigma.sigmathor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmathorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmathorApplication.class, args);
+	}
+
+}
